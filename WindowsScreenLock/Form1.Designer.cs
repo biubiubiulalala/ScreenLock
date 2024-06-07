f9baa2696afaf833be72dd26437143f4d655ffee
User <775057041@qq.com>
2024-06-07 10:51:52 +0800

    sync

--- WindowsScreenLock/Form1.Designer.cs
+++ WindowsScreenLock/Form1.Designer.cs
@@ -37,8 +37,9 @@
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button1.BackColor = System.Drawing.Color.LimeGreen;
             this.button1.Font = new System.Drawing.Font("华文行楷", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.ForeColor = System.Drawing.Color.FloralWhite;
             this.button1.Location = new System.Drawing.Point(104, 78);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(180, 60);
